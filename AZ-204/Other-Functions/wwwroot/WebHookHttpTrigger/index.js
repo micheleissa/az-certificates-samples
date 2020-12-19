32780658b579dcc277d9ccec6c1e2545eefce98a
Michel Eissa <michel.eissa85@gmail.com>
2020-12-19 15:11:55 -0600

    Add GitHub WebHook Function App

---
+++ AZ-204/Other-Functions/wwwroot/WebHookHttpTrigger/index.js
@@ -0,0 +1,30 @@
+const Crypto = require('crypto');
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const hmac = Crypto.createHmac("sha1", "default_key_here");
+    const signature = hmac.update(JSON.stringify(req.body)).digest('hex');
+    const shaSignature =  `sha1=${signature}`;
+    const gitHubSignature = req.headers['x-hub-signature'];
+
+    if (!shaSignature.localeCompare(gitHubSignature)) {
+        if (req.body.pages[0].title) {
+            context.res = {
+                body: "Page is " + req.body.pages[0].title + ", Action is " + req.body.pages[0].action + ", Event Type is " + req.headers['x-github-event']
+            };
+        }
+        else {
+            context.res = {
+                status: 400,
+                body: ("Invalid payload for Wiki event")
+            }
+        }
+    }
+    else {
+        context.res = {
+            status: 401,
+            body: "Signatures don't match"
+        };
+    }
+};
